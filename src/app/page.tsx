aec409e46cb22b0796b4bbe8594547d42a56c4ad
meanev56 <emekanjoku86@gmail.com>
2023-10-04 02:40:46 +0100

    env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1 className='text-red-600'>Hello world</h1>
+    
+    <Button>Click me</Button>
   )
 }
